bb9212eb68146dc173175fa75a9b5044f79a2387
Andrew Freebrey <mrmandrewman@users.noreply.github.com>
2019-01-27 08:35:39 +0000

    Refactor InGameMenu
    Renamed InGameMenu to ActorGameMenu.
    
    Created new folder Scripts\UI
    Moved Scripts\ActorGameMenu.cs to  Scripts\UI\ActorGameMenu.cs

--- Assets/Scripts/UI/ActorGameMenu.cs
+++ Assets/Scripts/UI/ActorGameMenu.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class InGameMenu : MonoBehaviour
+public class ActorGameMenu : MonoBehaviour
 {
 
     public GameObject PauseMenu;
